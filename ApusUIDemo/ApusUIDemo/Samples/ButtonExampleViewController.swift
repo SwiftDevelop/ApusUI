1b4c2e8930c4f49ff902f053de37361785aa7e04
SwiftDevelop <>
2025-10-28 16:25:51 +0900

    UIImageView Extension 추가

--- ApusUIDemo/ApusUIDemo/Samples/ButtonExampleViewController.swift
+++ ApusUIDemo/ApusUIDemo/Samples/ButtonExampleViewController.swift
@@ -10,33 +10,51 @@ import ApusUI
 
 final class ButtonExampleViewController: UIViewController {
     
+    private let iconImageView = UIImageView()
+    private let titleLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view
             .subviews {
                 UIButton()
                     .addAction { [weak self] button in
-                        button.isSelected.toggle()
-                        self?.changeBackgroundColor(button)
+                        self?.handleButtonAction(button)
+                    }
+                    .subviews {
+                        UIStackView(axis: .horizontal, alignment: .center, spacing: 4)
+                            .arrangedSubviews {
+                                iconImageView
+                                    .image(systemName: "lock.fill")
+                                    .tintColor(.white)
+                                titleLabel
+                                    .text("Lock")
+                                    .textColor(.white)
+                                    .font(size: 24, weight: .semibold)
+                            }
+                            .center()
                     }
                     .backgroundColor(.systemBlue)
-                    .title("Lock", for: .normal)
-                    .title("Unlock", for: .selected)
-                    .titleColor(.white)
-                    .font(size: 24, weight: .semibold)
-                    .image(systemName: "lock.fill", for: .normal)
-                    .image(systemName: "lock.open.fill", for: .selected)
-                    .tintColor(.white)
-                    .imageEdgeInsets(left: -2, right: 2)
-                    .titleEdgeInsets(left: 2, right: -2)
+//                    .title("Lock", for: .normal)
+//                    .title("Unlock", for: .selected)
+//                    .titleColor(.white)
+//                    .font(size: 24, weight: .semibold)
+//                    .image(systemName: "lock.fill", for: .normal)
+//                    .image(systemName: "lock.open.fill", for: .selected)
+//                    .tintColor(.white)
+//                    .imageEdgeInsets(left: -2, right: 2)
+//                    .titleEdgeInsets(left: 2, right: -2)
                     .frame(width: 200, height: 100)
                     .cornerRadius(16)
                     .center()
             }
     }
     
-    private func changeBackgroundColor(_ sender: UIButton) {
+    private func handleButtonAction(_ sender: UIButton) {
+        sender.isSelected.toggle()
         sender.backgroundColor(sender.isSelected ? .systemRed : .systemBlue)
+        iconImageView.image(systemName: sender.isSelected ? "lock.open.fill" : "lock.fill")
+        titleLabel.text(sender.isSelected ? "Unlock" : "Lock")
     }
 }
 
