3333c1f2e3d3c10cd8e968fe17bf036f1928acd5
SwiftDevelop <>
2025-11-04 17:11:01 +0900

    UISlider Extension 추가

---
+++ ApusUIDemo/ApusUIDemo/Samples/SliderExampleViewController.swift
@@ -0,0 +1,109 @@
+//
+//  SliderExampleViewController.swift
+//  ApusUI
+//
+//  Created by SwiftDevelop on 11/4/25.
+//
+
+import SwiftUI
+import ApusUI
+
+final class SliderExampleViewController: UIViewController {
+    
+    enum ColorSet: String, CaseIterable {
+        case red, green, blue
+        
+        var tintColor: UIColor {
+            switch self {
+            case .red:
+                return .red
+            case .green:
+                return .green
+            case .blue:
+                return .blue
+            }
+        }
+    }
+    
+    private let redColorValueLabel = UILabel()
+    private let greenColorValueLabel = UILabel()
+    private let blueColorValueLabel = UILabel()
+    private let colorView = UIView()
+    
+    private var red: CGFloat = 0
+    private var green: CGFloat = 0
+    private var blue: CGFloat = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.subviews {
+            UIStackView(.vertical, data: ColorSet.allCases) { colorSet in
+                makeColorSliderView(colorSet: colorSet)
+            }
+            .spacing(24)
+            .padding(top: 24)
+            .padding(horizontal: 16)
+            
+            colorView
+                .frame(height: 255)
+                .padding(bottom: 16)
+                .padding(horizontal: 16)
+        }
+        
+        updateUI()
+    }
+    
+    private func makeColorSliderView(colorSet: ColorSet) -> UIStackView {
+        UIStackView(.horizontal) {
+            UILabel(colorSet.rawValue)
+                .frame(width: 48)
+            
+            UISlider()
+                .thumbTintColor(colorSet.tintColor)
+                .minimumTrackTintColor(colorSet.tintColor)
+                .onChange { [weak self] value in
+                    let colorValue = CGFloat(value * 255)
+                    switch colorSet {
+                    case .red:
+                        self?.red = colorValue
+                    case .green:
+                        self?.green = colorValue
+                    case .blue:
+                        self?.blue = colorValue
+                    }
+                    
+                    self?.updateUI()
+                }
+            
+            makeValueLabel(colorSet: colorSet)
+                .frame(width: 40)
+        }
+        .spacing(16)
+    }
+    
+    private func makeValueLabel(colorSet: ColorSet) -> UILabel {
+        switch colorSet {
+        case .red:
+            redColorValueLabel
+        case .green:
+            greenColorValueLabel
+        case .blue:
+            blueColorValueLabel
+        }
+    }
+    
+    private func updateUI() {
+        redColorValueLabel.text("\(Int(red))")
+        greenColorValueLabel.text("\(Int(green))")
+        blueColorValueLabel.text("\(Int(blue))")
+        
+        colorView.backgroundColor(.init(red: red / 255, green: green / 255, blue: blue / 255, alpha: 1.0))
+    }
+}
+
+#Preview {
+    UIViewControllerPreview {
+        SliderExampleViewController()
+    }
+}
---
+++ Sources/ApusUI/Extensions/UISlider/UISlider+Extension.swift
@@ -0,0 +1,94 @@
+//
+//  UISlider+Extension.swift
+//  ApusUI
+//
+//  Created by SwiftDevelop on 11/4/25.
+//
+
+import UIKit
+
+// MARK: - AssociatedKey
+@MainActor private struct AssociatedKey {
+    static var sliderAction: UInt8 = 0
+}
+
+// MARK: - ActionWrapper
+private class ActionWrapper {
+    let action: (Float) -> Void
+    weak var control: UISlider?
+    
+    init(action: @escaping (Float) -> Void, control: UISlider?) {
+        self.action = action
+        self.control = control
+    }
+    
+    @MainActor @objc func invoke() {
+        action(control?.value ?? 0)
+    }
+}
+
+// MARK: - Extensions
+public extension UISlider {
+    @discardableResult
+    func value(_ value: Float, animated: Bool = false) -> Self {
+        self.setValue(value, animated: animated)
+        return self
+    }
+    
+    @discardableResult
+    func minimumValue(_ value: Float) -> Self {
+        self.minimumValue = value
+        return self
+    }
+    
+    @discardableResult
+    func maximumValue(_ value: Float) -> Self {
+        self.maximumValue = value
+        return self
+    }
+    
+    @discardableResult
+    func minimumTrackTintColor(_ color: UIColor?) -> Self {
+        self.minimumTrackTintColor = color
+        return self
+    }
+    
+    @discardableResult
+    func maximumTrackTintColor(_ color: UIColor?) -> Self {
+        self.maximumTrackTintColor = color
+        return self
+    }
+    
+    @discardableResult
+    func thumbTintColor(_ color: UIColor?) -> Self {
+        self.thumbTintColor = color
+        return self
+    }
+    
+    @discardableResult
+    func minimumTrackTintImage(_ image: UIImage?, for state: UIControl.State = .normal) -> Self {
+        self.setMinimumTrackImage(image, for: state)
+        return self
+    }
+    
+    @discardableResult
+    func maximumTrackTintImage(_ image: UIImage?, for state: UIControl.State = .normal) -> Self {
+        self.setMaximumTrackImage(image, for: state)
+        return self
+    }
+    
+    @discardableResult
+    func thumbImage(_ image: UIImage?, for state: UIControl.State = .normal) -> Self {
+        self.setThumbImage(image, for: state)
+        return self
+    }
+    
+    @discardableResult
+    func onChange(_ action: @escaping (Float) -> Void) -> Self {
+        let wrapper = ActionWrapper(action: action, control: self)
+        objc_setAssociatedObject(self, &AssociatedKey.sliderAction, wrapper, .OBJC_ASSOCIATION_RETAIN)
+        self.addTarget(wrapper, action: #selector(ActionWrapper.invoke), for: .valueChanged)
+        return self
+    }
+}
+
